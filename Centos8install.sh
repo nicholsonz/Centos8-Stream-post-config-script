fc0b555633548498b16ea6a22640ff7dfdb92894
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2022-01-15 14:36:04 -0600

    Update Centos8install.sh

--- Centos8install.sh
+++ Centos8install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 ##########################################################
-# Automated Centos 8 server installation and configuration
+# Automated RHEL based server installation and configuration
 ##########################################################
 
-# Set variables
+# System variables
 srvrname=
 dbuser=
 dbpasswd=
 adminUser=
-bkpdir=/mnt/backup/$(hostname)
-bkpdev=/dev/sdb
+smbuser=
+smbgrp=
+bkpdir=/mnt/backup/$srvrname
+# backup device
+bkpdev=/dev/sdb    
 ip=10.10.10.10
-# find uuid of backup device for fstab entry: "sudo blkid | grep UUID"
-uuid=
-
+# find uuid of backup device for fstab entry: "sudo blkid /dev/sd?"
+uuid=                         
+remivrsn=remi-release-8.rpm
+phpvrsn=7.4
 
 ###########################################################
 #
@@ -30,7 +34,7 @@ newaliases
 
 echo "$ip   $srvrname" >>/etc/hosts
 
-# Set SELINUX into permissive mode and permenantly disable
+# Set SELINUX into permissive mode and temporarily disable till end of script
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 
@@ -45,29 +49,31 @@ dnf update -y
 # Epel Repository installation
 dnf install -y epel-release
 dnf config-manager --set-enabled powertools
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+dnf install https://rpms.remirepo.net/enterprise/$remivrsn
 dnf update -y
 
 # Apache web server and security packages
-dnf install -y httpd mod_ssl openssl
-systemctl start httpd
+dnf install -y httpd mod_ssl openssl mod_security mod_security_crs
 
 # Mariadb install
 dnf install -y mariadb-server
-systemctl start mariadb
 
 # Extra packages
-dnf install -y lnav lm_sensors logwatch wget fail2ban fail2ban-systemd whois bash-completion NetworkManager-wifi NetworkManager-tui clamav clamav-update smartmontools
+dnf install -y lnav lm_sensors get whois bash-completion NetworkManager-wifi NetworkManager-tui smartmontools haveged goaccess
 
-# create log file for fail2ban before starting up
+# Security packages
+dnf install -y logwatch fail2ban fail2ban-systemd clamav clamav-update lynis
+
+# create log file for fail2ban before starting up to prevent start fail
 touch /var/log/fail2ban.log
 
 # Cockpit and related packages
 dnf install -y cockpit cockpit-packagekit cockpit-storaged cockpit-pcp
 
 # PHP install
-dnf module enable php:remi-7.4 
-dnf install -y php php-mysqlnd php-zip php-imap php-gd
+dnf module enable php:remi-$phpvrsn 
+dnf install -y php php-mysqlnd php-zip php-imap php-xml php-mbstring php-intl php-pear zip unzip git composer php-ldap php-imagick php-gd
+
 
 # PHPMYAdmin install and configure
 # all located in /var/www/html/.  just copy it and remember to put config file in /etc/httpd/conf.d of apache server
@@ -75,14 +81,29 @@ dnf install -y php php-mysqlnd php-zip php-imap php-gd
 # Postfix and Dovecot
 dnf install -y postfix postfix-mysql dovecot dovecot-mysql 
 
-# Install certbot 
+# Samba
+dnf install -y samba
+useradd -M -s /sbin/nologin $smbuser
+passwd $smbuser
+smbpasswd -a $smbuser
+groupadd $smbgrp
+usermod -aG $smbgrp $smbuser
+rsync -arv $bkpdir/etc/samba/ /etc/samba
+rsync -arv $bkpdir/srv/samba/ /srv/samba
+chmod -R 770 /srv/samba/*
+chown -R root:$smbgrp /srv/samba/*
+
+# Certbot 
 dnf install certbot python3-certbot-apache
+
+# Netdata for monitoring
+#curl -s https://packagecloud.io/install/repositories/netdata/netdata/script.rpm.sh | sudo bash
+#sudo dnf install netdata
 
 ###########################################################
 #
 #
 #### Begin system package restoration and configuration ####
-
 
 # Create holes in firewall
 firewall-cmd --permanent --add-service=imap
@@ -93,8 +114,8 @@ firewall-cmd --permanent --add-service=smtp
 firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
 firewall-cmd --permanent --add-service=cockpit
-firewall-cmd --permanent --add-port=10000/tcp
-firewall-cmd --permanent --add-port=19999/tcp
+firewall-cmd --permanent --add-service=samba
+#firewall-cmd --permanent --add-port=19999/tcp   #NetData
 firewall-cmd --permanent --add-port=2020/tcp
 firewall-cmd --permanent --add-port=587/tcp
 firewall-cmd --permanent --add-port=465/tcp
@@ -122,7 +143,7 @@ if ! mountpoint -q ${MNTPNT}/; then
 fi
 
 
-# Apache server configuration/restoration
+#### Apache server configuration/restoration ####
 
 echo "Begin Apache server configuration"
 echo
@@ -137,7 +158,17 @@ else
 
 fi
 
-# Mariadb config
+# restore config/dir files from backup or master server
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
+rsync -arv $bkpdir/etc/httpd/conf/httpd.conf /etc/httpd/conf
+rsync -arv $bkpdir/etc/httpd/conf.modules.d/ /etc/httpd/conf.modules.d
+rsync -arv $bkpdir/etc/httpd/modsecurity.d/ /etc/httpd/modsecurity.d
+rsync -arv $bkpdir/etc/httpd/conf.d/ /etc/httpd/conf.d
+rsync -arv $bkpdir/var/www/ /var/www
+rsync -arv $bkpdir/etc/smartmontools/smartd.conf /etc/smartmontools
+
+
+#### Mariadb config ####
 
 echo "Begin Mariadb configuration"
 echo
@@ -151,16 +182,14 @@ _EOF_
 echo "Listing of backed up databases:"
 echo "$(ls -I "*.log" $bkpdir/sql)"
 echo "-------------------------------------"
-echo "Name of databases seperated by spaces to restore?"
+echo "Enter name of databases seperated by spaces to restore?"
 read -p 'databases: ' dbases
 
 for dbase in $dbases
  do
-# adjust these variables for your setup
 DIR="$bkpdir/sql/${dbase}/"
 NEWEST=`ls -tr1d "${DIR}/"*.gz 2>/dev/null | tail -1`
 TODAY=$(date +"%a")
-#####################
 
 mysql --user=root -e "CREATE DATABASE $dbase DEFAULT CHARACTER SET utf8";
 
@@ -174,15 +203,11 @@ fi
 done
 
 echo "Securing SQL installation"
+
 mysql_secure_installation
 
-# restore config/dir files from backup or master server
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
-rsync -arv $bkpdir/etc/httpd/conf/httpd.conf /etc/httpd/conf
-rsync -arv $bkpdir/etc/httpd/conf.d/ /etc/httpd/conf.d
-rsync -arv $bkpdir/var/www/ /var/www
 
-# Postfix and Dovecot configuration
+#### Postfix and Dovecot configuration ####
 
 echo "Create virtual mail dir"
 mkdir /home/vmail
@@ -214,17 +239,20 @@ chown -R -o-rwx /etc/dovecot
 rsync -arv $bkpdir/home/$adminUser/ /home/$adminUser
 rsync -arv $bkpdir/etc/logwatch/ /etc/logwatch
 rsync -arv $bkpdir/etc/fail2ban/ /etc/fail2ban
+rsync -arv $bkpdir/etc/tripwire/ /etc/tripwire
+rsync -arv $bkpdir/etc/ssh/ /etc/ssh
 rsync -arv $bkpdir/etc/php.ini /etc
+rsync -arv $bkpdir/etc/goaccess/ /etc/goaccess
 rsync -arv $bkpdir/srv/ /srv
 
 # Webmin installation
-#{
+# {
 #  echo '[Webmin]'
 #  echo 'name=Webmin Distribution Neutral'
 #  echo '#baseurl=https://download.webmin.com/download/yum'
 #  echo 'mirrorlist=https://download.webmin.com/download/yum/mirrorlist'
 #  echo 'enabled=1'
-#} >/etc/yum.repos.d/webmin.repo
+# } >/etc/yum.repos.d/webmin.repo
 
 #rpm --import http://www.webmin.com/jcameron-key.asc
 #yum install -y webmin
@@ -249,21 +277,75 @@ systemctl start fail2ban
 systemctl enable fail2ban
 systemctl start clamav-freshclam
 systemctl enable clamav-freshclam
+systemctl start haveged
+systemctl enable haveged
+systemctl start smb
+systemctl enable smb
+systemctl start nmb
+systemctl enable nmb
 
-
-# Install NetData
+#### Install NetData ####
 #bash <(curl -Ss https://my-netdata.io/kickstart.sh)
 
 # Restore cronjobs
-rsync -arv $bkpdir/etc/cron.daily/ /etc/cron.daily
-cp $bkpdir/home/zach/root.crontab /var/spool/cron
-mv /var/spool/cron/root.crontab /var/spool/cron/root
-chmod 600 /var/spool/cron/root
+rsync -arv $bkpdir/etc/cron.custom /etc
+rsync -arv $bkpdir/etc/cron.daily /etc
+rsync -arv $bkpdir/etc/cron.weekly /etc
+crontab -u root /home/zach/repo/crontab.bak
 
-# RKHunter installation and update
+#### RKHunter installation and update ####
 dnf install -y rkhunter
+rsync -arv $bkpdir/etc/rkhunter.conf /etc
 rkhunter --update
 rkhunter --propupd
+
+# Install and initialize Tripwire
+dnf install -y tripwire
+
+# remove automatically generated daily check 
+rm /etc/cron.daily/tripwire-check
+tripwire --init
+# script to update database for missing dir/files 
+./tripwireupdate.sh
+
+# enable selinux and set policies
+setenforce 1
+sed -i 's/SELINUX=disabled/SELINUX=enforcing/' /etc/selinux/config
+setsebool -P domain_can_mmap_files 1
+setsebool -P httpd_unified 1
+# SELinux Automation script
+semanage import <<EOF
+boolean -D
+login -D
+interface -D
+user -D
+port -D
+node -D
+fcontext -D
+module -D
+ibendport -D
+ibpkey -D
+permissive -D
+boolean -m -1 domain_can_mmap_files
+boolean -m -1 httpd_unified
+boolean -m -1 nis_enabled
+boolean -m -1 samba_export_all_ro
+boolean -m -1 samba_export_all_rw
+boolean -m -1 virt_sandbox_use_all_caps
+boolean -m -1 virt_use_nfs
+fcontext -a -f a -t httpd_sys_content_t -r 's0' '/srv/pfxadmin'
+fcontext -a -f a -t samba_share_t -r 's0' '/srv/samba'
+fcontext -a -f a -t httpd_sys_rw_content_t -r 's0' 'twig'
+EOF
+
+
+# set persistent file types for PostfixAdmin and Samba dirs/files
+semanage fcontext -a -t samba_share_t /srv/samba
+semanage fcontext -a -t httpd_sys_content_t /srv/pfxadmin
+
+# not persistant make sure /srv directory has correct file types set for PostfixAdmin and Samba
+#chcon -Rv --type=samba_share_t /srv/samba
+#chcon -Rv --type=httpd_sys_content_t /srv/pfxadmin 
 
 echo "All Finished!  The computer will now reboot."
 
